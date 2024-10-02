9ba4bd6c32fc1340bd9060ce205d05096909b374
mayura-andrew <mayuraalahakoon@gmail.com>
2024-10-02 13:29:31 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Button } from './components/ui/button'
+import './index.css'
+import Home from './pages/home'
+
+
+
+
+
+
+
+
+
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Button> Click me </Button>
+      </div>
+      <Home />
+    </>
+  )
+}
+
+export default App
