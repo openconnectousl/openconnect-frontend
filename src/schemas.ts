36dec4c5f2ffab92b32872114f52c997e9600e95
pasindu526 <pasindumadusanka526@gmail.com>
2024-10-10 15:36:59 +0530

    Create a Verification component and integrate with API.

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
-export const SignUpFormSchema = z.object({
-    email: z.string().email("Invalid email").min(1, "Required"),
-    password: z.string().min(8, "Minimum 8 characters").max(100, "Maximum 100 characters"),
-})
-
+export const VerificationFormSchema = z.object({
+    verificationCode: z
+        .string()
+        .nonempty("Verification Code is Required")
+        .min(26, "Verification Code must be at least 26 characters"),
+});
