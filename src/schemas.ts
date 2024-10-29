60164d17fa2cf21440646f8002b1dcca903460aa
Ashan Vimukthi <ashanwithanarachchi@gmail.com>
2024-10-29 18:01:58 +0530

    Update schemas.ts
    add SignInFormSchema validations

--- src/schemas.ts
+++ src/schemas.ts
@@ -11,3 +11,13 @@ export const SignUpFormSchema = z.object({
     email: z.string().email("Invalid email").min(1, "Required"),
     password: z.string().min(8, "Minimum 8 characters").max(100, "Maximum 100 characters"),
 })
+
+export const SignInFormSchema = z.object({
+    email: z.string()
+        .email("Invalid email address")
+        .min(1, "Email is required"),
+    
+        password: z.string()
+            .min(8, "Password reuirred at least 8 characters")
+            .max(100, "Password must be less than 100 characters"),
+});
