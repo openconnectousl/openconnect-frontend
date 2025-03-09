2aa9e335530fe14261ee9e358614e2094c7ef15f
Mayura Andrew <48531182+mayura-andrew@users.noreply.github.com>
2025-03-09 09:15:16 +0530

    Revert "Implement Authentication Flow with Custom Hooks and Type-safe API Integration" (#20)

--- src/pages/Dashboard/Features/CreateEvents/index.tsx
+++
@@ -1,9 +0,0 @@
-const CreateEvents: React.FC = () => {
-    return (
-        <div className="container mx-auto p-6">
-            <h1>Create Events</h1>
-        </div>
-    )
-}
-
-export default CreateEvents
--- src/pages/Dashboard/Features/ManageProjects/index.tsx
+++
@@ -1,5 +0,0 @@
-const ManageProjects: React.FC = () => {
-    return <div>Manage Projects</div>
-}
-
-export default ManageProjects
